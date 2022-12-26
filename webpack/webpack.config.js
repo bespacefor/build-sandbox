7b3fe67142305ebbfd0b55bec439b413ab6262a1
Sveta <svet.lukyantseva@gmail.com>
2022-12-26 23:59:23 +0300

    feat: add file-types

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,13 +3,25 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.png$/,
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             outputPath: 'images',
                             name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
                         }
                     }
                 ]
