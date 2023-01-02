08f3369353f6ae2622e0597c809ca56ac9c4f89e
Sveta <svet.lukyantseva@gmail.com>
2023-01-02 23:25:44 +0300

    feat: use html-webpack-plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: "development",
     module: {
@@ -48,5 +50,10 @@ module.exports = {
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
 };
